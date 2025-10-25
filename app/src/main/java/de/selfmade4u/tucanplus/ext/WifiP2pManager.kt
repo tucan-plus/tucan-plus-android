c7c1612eb216aefa7e3d5b1238ef31d1db4dd5a9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-10-26 00:00:04 +0200

    wip

--- app/src/main/java/de/selfmade4u/tucanplus/ext/WifiP2pManager.kt
+++ app/src/main/java/de/selfmade4u/tucanplus/ext/WifiP2pManager.kt
@@ -5,6 +5,8 @@ import android.net.wifi.p2p.WifiP2pDevice
 import android.net.wifi.p2p.WifiP2pManager
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest
 import android.net.wifi.p2p.nsd.WifiP2pServiceInfo
+import android.util.Log
+import de.selfmade4u.tucanplus.LocalNetworkNSD.Companion.TAG
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
@@ -21,10 +23,12 @@ import kotlin.coroutines.resumeWithException
 suspend fun WifiP2pManager.addLocalService(channel: WifiP2pManager.Channel, serviceInfo: WifiP2pServiceInfo): Closeable = suspendCancellableCoroutine { continuation ->
     val listener = object : WifiP2pManager.ActionListener {
         override fun onFailure(reason: Int) {
+            Log.d(TAG, "addLocalService onFailure $reason")
             continuation.resumeWithException(Exception("addLocalService failed with reason $reason"))
         }
 
         override fun onSuccess() {
+            Log.d(TAG, "addLocalService onSuccess")
             val self = this
             continuation.resume(object : Closeable {
                 override fun close() {
@@ -43,9 +47,11 @@ fun WifiP2pManager.setDnsSdResponseListenersFlow(channel: WifiP2pManager.Channel
     var discoveredServices: List<WifiP2pDevice> = listOf()
     setDnsSdResponseListeners(channel,
         { instanceName, registrationType, srcDevice ->
+            Log.d(TAG, "setDnsSdResponseListenersFlow $instanceName $registrationType $srcDevice")
             discoveredServices = discoveredServices + srcDevice;
             trySendBlocking(discoveredServices) }
     ) { fullDomainName, txtRecordMap, srcDevice ->
+        Log.d(TAG, "setDnsSdResponseListenersFlow $fullDomainName $txtRecordMap $srcDevice")
         discoveredServices = discoveredServices + srcDevice;
         trySendBlocking(discoveredServices)
     }
@@ -61,10 +67,12 @@ fun WifiP2pManager.setDnsSdResponseListenersFlow(channel: WifiP2pManager.Channel
 suspend fun WifiP2pManager.addServiceRequest(channel: WifiP2pManager.Channel, request: WifiP2pDnsSdServiceRequest): Closeable = suspendCancellableCoroutine { continuation ->
     val listener = object : WifiP2pManager.ActionListener {
         override fun onFailure(reason: Int) {
+            Log.d(TAG, "addServiceRequest onFailure $reason")
             continuation.resumeWithException(Exception("addServiceRequest failed with reason $reason"))
         }
 
         override fun onSuccess() {
+            Log.d(TAG, "addServiceRequest onSuccess")
             val self = this
             continuation.resume(object : Closeable {
                 override fun close() {
@@ -83,10 +91,12 @@ suspend fun WifiP2pManager.addServiceRequest(channel: WifiP2pManager.Channel, re
 suspend fun WifiP2pManager.discoverServices(channel: WifiP2pManager.Channel): Closeable = suspendCancellableCoroutine { continuation ->
     discoverServices(channel, object : WifiP2pManager.ActionListener {
         override fun onFailure(reason: Int) {
+            Log.d(TAG, "discoverServices onFailure $reason")
             continuation.resumeWithException(Exception("discoverServices failed with reason $reason"))
         }
 
         override fun onSuccess() {
+            Log.d(TAG, "discoverServices onSuccess")
             continuation.resume(object : Closeable {
                 override fun close() {
                     clearServiceRequests(channel, object : WifiP2pManager.ActionListener {
