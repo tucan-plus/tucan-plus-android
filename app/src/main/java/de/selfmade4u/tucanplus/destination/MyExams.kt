813cef85978ea25553332b05294be51d5f9e7461
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-11-18 23:07:46 +0100

    Try calendar stuff

--- app/src/main/java/de/selfmade4u/tucanplus/destination/MyExams.kt
+++ app/src/main/java/de/selfmade4u/tucanplus/destination/MyExams.kt
@@ -1,12 +1,22 @@
 package de.selfmade4u.tucanplus.destination
 
+import android.Manifest
+import android.accounts.Account
+import android.content.ContentResolver
+import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Intent
+import android.content.SyncRequest
 import android.content.res.Configuration
+import android.database.ContentObserver
 import android.database.Cursor
 import android.net.Uri
+import android.os.Handler
 import android.provider.CalendarContract
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -24,12 +34,14 @@ import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,6 +60,8 @@ import de.selfmade4u.tucanplus.connector.Semester
 import de.selfmade4u.tucanplus.connector.Semesterauswahl
 import de.selfmade4u.tucanplus.credentialSettingsDataStore
 import de.selfmade4u.tucanplus.data.MyExams
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Calendar
 
 
@@ -55,92 +69,127 @@ import java.util.Calendar
 @Composable
 fun MyExamsComposable(backStack: NavBackStack<NavKey>, isLoading: MutableState<Boolean>) {
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val launcher =
+        rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { value ->
+            Toast.makeText(context, "Permission response $value", Toast.LENGTH_SHORT).show()
 
-    val CALENDAR_PROJECTION: Array<String> = arrayOf(
-        CalendarContract.Calendars._ID,                     // 0
-        CalendarContract.Calendars.NAME,            // 1
-    )
-    val PROJECTION_CALENDAR_ID_INDEX: Int = 0
-    val PROJECTION_CALENDAR_NAME_INDEX: Int = 1
-    var cur: Cursor = context.contentResolver.query(CalendarContract.Calendars.CONTENT_URI, CALENDAR_PROJECTION, null, null, null)!!
-    while (cur.moveToNext()) {
-        val calendarId: Long = cur.getLong(PROJECTION_CALENDAR_ID_INDEX)
-        val name: String = cur.getString(PROJECTION_CALENDAR_NAME_INDEX)
-        Log.w(
-            TAG,
-            "GOT calendar $calendarId $name"
-        )
-    }
-    cur.close()
+            context.contentResolver.registerContentObserver(CalendarContract.Events.CONTENT_URI, true, object : ContentObserver(
+                Handler()) {
+                override fun onChange(selfChange: Boolean, uri: Uri?) {
+                    Log.e(TAG, "ONCHANGE $uri")
+                }
 
-    val calID: Long = 3
-    val startMillis: Long = Calendar.getInstance().run {
-        set(2025, 9, 14, 7, 30)
-        timeInMillis
-    }
-    val endMillis: Long = Calendar.getInstance().run {
-        set(2025, 9, 14, 8, 45)
-        timeInMillis
-    }
-    val values = ContentValues().apply {
-        put(CalendarContract.Events.DTSTART, startMillis)
-        put(CalendarContract.Events.DTEND, endMillis)
-        put(CalendarContract.Events.TITLE, "abcdef")
-        put(CalendarContract.Events.DESCRIPTION, "Group workout")
-        put(CalendarContract.Events.CALENDAR_ID, calID) // FIXME
-        put(CalendarContract.Events.EVENT_TIMEZONE, "America/Los_Angeles")
-        put(CalendarContract.Events.CUSTOM_APP_PACKAGE, context.packageName)
-        put(CalendarContract.Events.CUSTOM_APP_URI, "${context.packageName}://test")
-    }
-    var uri: Uri = context.contentResolver.insert(CalendarContract.Events.CONTENT_URI, values)!!
 
-    // get the event ID that is the last element in the Uri
-    val eventID: Long = uri.lastPathSegment!!.toLong()
-    //
-    // ... do something with event ID
-    //
-    //
-    Log.w(
-        TAG,
-        "inserted $eventID $uri"
-    )
+            })
 
-    val EVENT_PROJECTION: Array<String> = arrayOf(
-        CalendarContract.Events._ID,                     // 0
-        CalendarContract.Events.TITLE,            // 1
-        CalendarContract.Events.DESCRIPTION,   // 2
-        CalendarContract.Events.EVENT_LOCATION,            // 3
-        CalendarContract.Events.CUSTOM_APP_PACKAGE,            // 4
-        CalendarContract.Events.CUSTOM_APP_URI,            // 5
-    )
-    val PROJECTION_ID_INDEX: Int = 0
-    val PROJECTION_TITLE_INDEX: Int = 1
-    val PROJECTION_DESCRIPTION_INDEX: Int = 2
-    val PROJECTION_EVENT_LOCATION_INDEX: Int = 3
-    val PROJECTION_CUSTOM_APP_PACKAGE_INDEX: Int = 4
-    val PROJECTION_CUSTOM_APP_URI_INDEX: Int = 5
-    cur = context.contentResolver.query(CalendarContract.Events.CONTENT_URI, EVENT_PROJECTION, null, null, null)!!
-    while (cur.moveToNext()) {
-        val eventId: Long = cur.getLong(PROJECTION_ID_INDEX)
-        val title: String = cur.getString(PROJECTION_TITLE_INDEX)
-        val description: String = cur.getString(PROJECTION_DESCRIPTION_INDEX)
-        val location: String? = cur.getStringOrNull(PROJECTION_EVENT_LOCATION_INDEX)
-        val customAppPackage: String? = cur.getStringOrNull(PROJECTION_CUSTOM_APP_PACKAGE_INDEX)
-        val customAppUri: String? = cur.getStringOrNull(PROJECTION_CUSTOM_APP_URI_INDEX)
-        if (customAppPackage != null) {
+            Log.e(TAG, "DELETING ALL OUR OLD EVENTS")
+            context.contentResolver.delete(CalendarContract.Events.CONTENT_URI, "${CalendarContract.Events.CUSTOM_APP_PACKAGE} = ?", arrayOf(context.packageName))
+
+            val CALENDAR_PROJECTION: Array<String> = arrayOf(
+                CalendarContract.Calendars._ID,                     // 0
+                CalendarContract.Calendars.NAME,            // 1
+                CalendarContract.Calendars.ACCOUNT_NAME, // 2
+                CalendarContract.Calendars.ACCOUNT_TYPE // 3
+            )
+            val PROJECTION_CALENDAR_ID_INDEX: Int = 0
+            val PROJECTION_CALENDAR_NAME_INDEX: Int = 1
+            val PROJECTION_CALENDAR_ACCOUNT_NAME_INDEX: Int = 2
+            val PROJECTION_CALENDAR_ACCOUNT_TYPE_INDEX: Int = 3
+            var cur: Cursor = context.contentResolver.query(CalendarContract.Calendars.CONTENT_URI, CALENDAR_PROJECTION, null, null, null)!!
+            Log.w(TAG, "CALENDARS")
+            while (cur.moveToNext()) {
+                val calendarId: Long = cur.getLong(PROJECTION_CALENDAR_ID_INDEX)
+                val name: String = cur.getString(PROJECTION_CALENDAR_NAME_INDEX)
+                val accountName: String = cur.getString(PROJECTION_CALENDAR_ACCOUNT_NAME_INDEX)
+                val accountType: String = cur.getString(PROJECTION_CALENDAR_ACCOUNT_TYPE_INDEX)
+                Log.w(
+                    TAG,
+                    "GOT calendar $calendarId $name $accountName $accountType"
+                )
+            }
+            cur.close()
+
+            val calID: Long = 3
+            val startMillis: Long = Calendar.getInstance().run {
+                set(2025, 9, 14, 7, 30)
+                timeInMillis
+            }
+            val endMillis: Long = Calendar.getInstance().run {
+                set(2025, 9, 14, 8, 45)
+                timeInMillis
+            }
+            val values = ContentValues().apply {
+                put(CalendarContract.Events.DTSTART, startMillis)
+                put(CalendarContract.Events.DTEND, endMillis)
+                put(CalendarContract.Events.TITLE, "abcdef")
+                put(CalendarContract.Events.DESCRIPTION, "Group workout")
+                put(CalendarContract.Events.CALENDAR_ID, calID) // FIXME
+                put(CalendarContract.Events.EVENT_TIMEZONE, "America/Los_Angeles")
+                put(CalendarContract.Events.CUSTOM_APP_PACKAGE, context.packageName)
+                put(CalendarContract.Events.CUSTOM_APP_URI, "${context.packageName}://test")
+            }
+            var uri: Uri = context.contentResolver.insert(CalendarContract.Events.CONTENT_URI, values)!!
+
+            // https://stackoverflow.com/questions/61012807/android-calendar-provider-does-not-return-latest-data
+            // https://stackoverflow.com/questions/79314548/how-to-get-updated-information-from-a-contentprovider-android-development
+            ContentResolver.requestSync(SyncRequest.Builder().setSyncAdapter(Account("mrtzndrdmltr@gmail.com", "com.google"), uri.authority).setExpedited(true).setManual(true).syncOnce().build());
+
+            // get the event ID that is the last element in the Uri
+            val eventID: Long = uri.lastPathSegment!!.toLong()
+            //
+            // ... do something with event ID
+            //
+            //
             Log.w(
                 TAG,
-                "GOT AN EVENT $eventId $title $description $location $customAppPackage $customAppUri"
+                "inserted $eventID $uri"
             )
-        }
-    }
-    cur.close()
 
-    // TODO I think this needs a delay for some reason
-    // maybe the insertion could also use a callback or so? but the query lists it so idk
-    //uri = ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, 232)
-    val intent = Intent(Intent.ACTION_VIEW).setData(uri)
-    context.startActivity(intent)
+            val EVENT_PROJECTION: Array<String> = arrayOf(
+                CalendarContract.Events._ID,                     // 0
+                CalendarContract.Events.TITLE,            // 1
+                CalendarContract.Events.DESCRIPTION,   // 2
+                CalendarContract.Events.EVENT_LOCATION,            // 3
+                CalendarContract.Events.CUSTOM_APP_PACKAGE,            // 4
+                CalendarContract.Events.CUSTOM_APP_URI,            // 5
+            )
+            val PROJECTION_ID_INDEX: Int = 0
+            val PROJECTION_TITLE_INDEX: Int = 1
+            val PROJECTION_DESCRIPTION_INDEX: Int = 2
+            val PROJECTION_EVENT_LOCATION_INDEX: Int = 3
+            val PROJECTION_CUSTOM_APP_PACKAGE_INDEX: Int = 4
+            val PROJECTION_CUSTOM_APP_URI_INDEX: Int = 5
+            cur = context.contentResolver.query(CalendarContract.Events.CONTENT_URI, EVENT_PROJECTION, null, null, null)!!
+            while (cur.moveToNext()) {
+                val eventId: Long = cur.getLong(PROJECTION_ID_INDEX)
+                val title: String = cur.getString(PROJECTION_TITLE_INDEX)
+                val description: String = cur.getString(PROJECTION_DESCRIPTION_INDEX)
+                val location: String? = cur.getStringOrNull(PROJECTION_EVENT_LOCATION_INDEX)
+                val customAppPackage: String? = cur.getStringOrNull(PROJECTION_CUSTOM_APP_PACKAGE_INDEX)
+                val customAppUri: String? = cur.getStringOrNull(PROJECTION_CUSTOM_APP_URI_INDEX)
+                if (customAppPackage != null) {
+                    Log.w(
+                        TAG,
+                        "GOT AN EVENT $eventId $title $description $location $customAppPackage $customAppUri"
+                    )
+                }
+            }
+            cur.close()
+
+            scope.launch {
+                delay(10000)
+
+                // TODO I think this needs a delay for some reason
+                // maybe the insertion could also use a callback or so? but the query lists it so idk
+                uri = ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, 174)
+                val intent = Intent(Intent.ACTION_VIEW).setData(uri)
+                context.startActivity(intent)
+            }
+        }
+    LaunchedEffect(true) {
+        launcher.launch(arrayOf(Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR))
+    }
 /*
     val beginTime: Calendar = Calendar.getInstance()
     beginTime.set(2012, 0, 19, 7, 30)
